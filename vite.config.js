1a6802ddc104683c7e8d4010ebe1335bb73610e5
Esraa salah <83188533+esraamajed@users.noreply.github.com>
2025-10-03 02:09:13 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-base: '/react-product-catalog/',
-})
+ base: '/-small-Product-Catalog-/'})
