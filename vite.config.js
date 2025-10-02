142ecc9771f86c826a078887212ed4b2593aa0b4
esraamajed <accesraa@outlook.com>
2025-10-03 02:49:37 +0400

    new

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/react-product-catalog/',
+   base: '/-small-Product-Catalog-/',
   plugins: [react()],
  })
